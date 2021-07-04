0560ceb353ffda89c24fa04a3c265370a544e190
The Kien (Ken) Nguyen <60043570+kennguyen0303@users.noreply.github.com>
2021-07-04 09:03:14 -0400

    Added code editor to query tab (#7)
    * Added code editor to query tab
    
    * cleaned up

--- front-end/src/views/Result/TabQuery.js
+++ front-end/src/views/Result/TabQuery.js
@@ -1,10 +1,26 @@
-import { Box, Card } from "@material-ui/core";
-import React from "react";
+import { Box } from '@material-ui/core';
+import React from 'react';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+
+require('codemirror/lib/codemirror.css');
+require('codemirror/theme/material.css');
+require('codemirror/theme/neat.css');
+require('codemirror/mode/sparql/sparql.js');
 
 const QueryTab = ({ query, queryKeywords }) => {
   return (
-    <Box mb={7}>
-      <Card>{query}</Card>
+    <Box mb={7} style={{ height: '500px' }}>
+      <CodeMirror
+        value={query}
+        options={{
+          mode: 'sparql',
+          theme: 'material',
+          lineNumbers: true
+        }}
+        onChange={(editor, data, value) => {
+          // Place to add actions when code is modified
+        }}
+      />
     </Box>
   );
 };
