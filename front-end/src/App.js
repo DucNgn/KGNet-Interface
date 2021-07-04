9b4cc2bc9e495549b69801601368172a4a4abc8c
Ken Nguyen <kenn.tnguyen@gmail.com>
2021-07-04 16:28:52 -0400

    updated view on small screen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme) => ({
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('sm')]: {
       marginLeft: 250,
       marginTop: 50
     },
