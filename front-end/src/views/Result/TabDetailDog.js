b9393e2043ff0e9e3bb5b601891297a24d60323a
DukeNgn <triduc2311m@gmail.com>
2021-07-04 20:11:15 -0400

    Move SHAP graph in Dog Breed Result page to the bottom

--- front-end/src/views/Result/TabDetailDog.js
+++ front-end/src/views/Result/TabDetailDog.js
@@ -1,39 +1,39 @@
-import { Box, Card, makeStyles, Typography } from '@material-ui/core';
-import React from 'react';
+import { Box, Card, makeStyles, Typography, Grid } from "@material-ui/core";
+import React from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
   image: {
-    [theme.breakpoints.down('sm')]: {
-      height: '30%',
-      weight: '30%'
+    [theme.breakpoints.down("sm")]: {
+      height: "30%",
+      weight: "30%",
     },
-    [theme.breakpoints.up('sm')]: {
-      height: '30%',
-      width: '30%'
+    [theme.breakpoints.up("sm")]: {
+      height: "30%",
+      width: "30%",
     },
-    [theme.breakpoints.up('md')]: {
-      height: '20%',
-      width: '20%'
+    [theme.breakpoints.up("md")]: {
+      height: "20%",
+      width: "20%",
     },
-    [theme.breakpoints.up('lg')]: {
-      height: '10%',
-      width: '10%'
-    }
+    [theme.breakpoints.up("lg")]: {
+      height: "10%",
+      width: "10%",
+    },
   },
   bold: {
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   shapImage: {
-    [theme.breakpoints.down('sm')]: {
-      height: '80%',
-      weight: '80%'
+    [theme.breakpoints.down("sm")]: {
+      height: "80%",
+      weight: "80%",
     },
-    [theme.breakpoints.up('sm')]: {
-      height: '100%',
-      width: '100%'
-    }
-  }
+    [theme.breakpoints.up("sm")]: {
+      height: "100%",
+      width: "100%",
+    },
+  },
 }));
 const TabDetailDog = ({ mode, dogDetail }) => {
   const classes = useStyles();
@@ -42,14 +42,18 @@ const TabDetailDog = ({ mode, dogDetail }) => {
     <Box mb={7}>
       <Card>
         <Box m={2}>
-          <img src={`${dogDetail.dog_image}`} alt="Dog Breed" className={classes.image} />
+          <img
+            src={`${dogDetail.dog_image}`}
+            alt="Dog Breed"
+            className={classes.image}
+          />
           <Box my={1} />
           <Typography variant="h5" color="textPrimary">
             <span className={classes.bold}>Breed's name: </span>
             {dogDetail.breed_class}
           </Typography>
           <Box my={1} />
-          {mode === 'dogSimilarity' ? (
+          {mode === "dogSimilarity" ? (
             <Box>
               <Box display="flex" alignItems="center">
                 <Typography variant="body1" color="textPrimary">
@@ -62,7 +66,6 @@ const TabDetailDog = ({ mode, dogDetail }) => {
               </Box>
             </Box>
           ) : null}
-          <img src={`${dogDetail.SHAPFigure}`} alt="SHAP Explainer" className={classes.shapImage} />
           <Typography variant="h5" color="textPrimary">
             Overview
           </Typography>
@@ -122,6 +125,19 @@ const TabDetailDog = ({ mode, dogDetail }) => {
             </Typography>
           </Box>
           <Box my={1} />
+          <Typography variant="body1" color="textPrimary">
+            <span className={classes.bold}>SHAP Explainer graph:</span>
+          </Typography>
+          <Grid direction="column" alignItems="center" xs={12} container>
+            <img
+              src={`${dogDetail.SHAPFigure}`}
+              alt="SHAP Explainer"
+              className={classes.shapImage}
+            />
+            <Typography variant="caption" color="textSecondary">
+              {dogDetail.SHAPDescription}
+            </Typography>
+          </Grid>
         </Box>
       </Card>
     </Box>
