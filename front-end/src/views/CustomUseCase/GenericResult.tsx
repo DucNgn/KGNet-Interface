82a75fd55b2dbb4f775b51101d179ca1e824408f
Ken Nguyen <kenn.tnguyen@gmail.com>
2021-09-08 23:31:04 -0400

    debug

--- front-end/src/views/CustomUseCase/GenericResult.tsx
+++ front-end/src/views/CustomUseCase/GenericResult.tsx
@@ -17,6 +17,7 @@ const GenericResult: React.FunctionComponent<Props> = ({ result }) => {
       <Typography variant="h4" color="textPrimary">
         Result for your query:
       </Typography>
+      {console.log(result)}
       {result.map((res: any, idx: number) => {
         <Box>
           <Typography variant="body1" color="textPrimary">
